490c058b04784f84a0c4b55ad5b32254bc2b4f80
Rajrupa Sanyal <rajrupasanyal@gmail.com>
2024-11-10 18:57:54 -0800

    changes made in test.js based on userdata change

--- backend/test.js
+++ backend/test.js
@@ -17,15 +17,9 @@ const textResponse = "I wanted the layout to prioritize green spaces and accessi
 app.post('/save-game', async (req, res) => {
     const gameData = req.body;
 
-    // Construct drag actions summary based on game data
-    const actionsSummary = gameData.flows.map(flow => 
-      `Placed a ${flow.color} flow from ${flow.start} to ${flow.end}. Path: ${flow.path.length} steps.`
-    ).join(' ');
-
-  
 
     // Now you don't have the long answer question anymore, but you can still use the game stats
-    const { totalTime, totalMoves, correctMoves, incorrectMoves, completed } = gameData.gameStats;
+    const { totalTime, totalMoves, correctMoves, incorrectMoves, completed } = gameData;
 
     const prompt = `
     The user has completed a puzzle with the following actions: ${actionsSummary}.
